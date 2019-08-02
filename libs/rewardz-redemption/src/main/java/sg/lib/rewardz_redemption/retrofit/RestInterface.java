83cd9fa9f59705728e74020c485db062aee6ca2c
Yongqi Yan <35778262+Yan19910828@users.noreply.github.com>
2019-08-02 11:02:51 -0400

    Add files via upload

---
+++ libs/rewardz-redemption/src/main/java/sg/lib/rewardz_redemption/retrofit/RestInterface.java
@@ -0,0 +1,37 @@
+package sg.lib.rewardz_redemption.retrofit;
+
+import java.util.Map;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Url;
+
+/**
+ * Created by manishandroid on 06/05/16.
+ *
+ * interface that includes all the methods
+ * which are used for making Retrofit API calls.
+ *
+ */
+public interface RestInterface {
+
+    @FormUrlEncoded
+    @POST(GlobalVariables.LOGIN_API_URL)
+    Call<String> login(@FieldMap Map<String, String> map);
+
+    @GET
+    Call<String> getRewards(@Header("Authorization") String token, @Url String nextOrPrevUrl);
+
+    @GET
+    Call<String> getAllRewards(@Header("Authorization") String token, @Url String nextOrPrevUrl);
+
+    @FormUrlEncoded
+    @POST(GlobalVariables.REDEEM_REWARD_API_URL)
+    Call<ResponseBody> redeemReward(@Header("Authorization") String token, @FieldMap Map<String, String> map);
+
+}
